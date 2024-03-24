4c7f3d05fad4f3f8e7845642cb588cd4b6167251
Hana Nur Aisyah <hananurtyn@gmail.com>
2024-03-24 23:06:41 +0700

    edit text di hero lagii

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
         <div className="hero min-h-screen z-0" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1588668214407-6ea9a6d8c272?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md  text-slate-900 ">
+    <div className="max-w-md  text-slate-200">
       <h1 className="mb-5 sm:text-6xl text-3xl font-bold">Let's have unforgettable</h1>
       <p className="mb-5">
         <span>Find your perfect escape to Indonesia's best paradises! {" "}</span>
